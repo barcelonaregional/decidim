a4b39785a87e9cd5ca5110ba9855fda730854e18
Ivan Vergés <ivan@platoniq.net>
2019-02-18 13:59:23 +0100

    remove es language

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -6,7 +6,7 @@ Decidim.configure do |config|
 
   # Change these lines to set your preferred locales
   config.default_locale = :ca
-  config.available_locales = [:es, :ca]
+  config.available_locales = [:ca]
 
   # Geocoder configuration
   config.geocoder = {
